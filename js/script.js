510cc4f47b959be371b19a9ef06869b2c44dfb97
merlinovvv <merlinovandrej@gmail.com>
2022-10-24 20:03:31 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
 "use strict"
+var t;
+function up() {
+    if ((window.innerHeight + window.pageYOffset) < document.documentElement.scrollHeight - 1) 
+    { 
+        window.scrollBy(0, 100); 
+        t = setTimeout('up()', 20); 
+    } else clearTimeout(t);
+    console.log(window.innerHeight); 
+    console.log(window.pageYOffset);
+    console.log(window.document.documentElement.scrollHeight); 
+    return false;
+}
+
 var im = new Inputmask("+38 (999) 999-99-99");
 
 var mainForm = document.querySelector('.form__profile');
-var telMain = mainForm.querySelector('input[type="tel"]');
-im.mask(telMain);
+if (mainForm !== null) {
+    var telMain = mainForm.querySelector('input[type="tel"]');
+    im.mask(telMain);
+}
 
 var slideForm = document.querySelector('.questions__form');
-var telSlide = slideForm.querySelector('input[type="tel"]');
-im.mask(telSlide);
+if (slideForm !== null) {
+    var telSlide = slideForm.querySelector('input[type="tel"]');
+    im.mask(telSlide);
+}
 
 let myElement = document.querySelector('.questions__form');
 let btnSend = document.querySelector('.btn-send');
@@ -19,7 +36,6 @@ function next() {
         myElement.children[j].classList.remove('on');
         myElement.children[j + 1].classList.add('on');
         j++;
-        console.log(arr);
     }
 }
 function back() {
@@ -29,102 +45,104 @@ function back() {
         j--;
     }
 }
-
-new window.JustValidate('.questions__form', {
-    rules: {
-        tel: {
-            required: true,
-            function: () => {
-                const phone = telSlide.inputmask.unmaskedvalue();
-                return Number(phone) && phone.length === 10;
-            }
-        }
-    },
-    messages: {
-        name: {
-            required: 'Введіть ім`я',
-            minLength: 'Введіть мінімум 3 символи',
-            maxLength: 'Максимальна кількість символів - 15'
-        },
-        tel: {
-            required: 'Введіть телефон',
-            function: 'Введіть коректний телефон'
-        },
-        radio:{
-            required: 'Виберіть один із запропонованих пунктів'
-        }
-    },
-    colorWrong: 'red',
-    submitHandler: function (thisForm) {
-        let formData = new FormData(thisForm);
-        
-        let xhr = new XMLHttpRequest();
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    setTimeout(
-                        () => {
-                            myElement.children[j].classList.remove('on');
-                            myElement.children[j + 1].classList.add('on');
-                            j++;
-                        },
-                        2 * 10
-                      );
+if (slideForm !== null) {
+    new window.JustValidate('.questions__form', {
+        rules: {
+            tel: {
+                required: true,
+                function: () => {
+                    const phone = telSlide.inputmask.unmaskedvalue();
+                    return Number(phone) && phone.length === 10;
                 }
             }
-        }
-        xhr.open('POST', 'mail.php', true);
-        xhr.send(formData);
-
-        thisForm = reset();
-    }
-});
-
-new window.JustValidate('.form__profile', {
-    rules: {
-        tel: {
-            required: true,
-            function: () => {
-                const phone = telMain.inputmask.unmaskedvalue();
-                return Number(phone) && phone.length === 10;
-            }
-        }
-    },
-    messages: {
-        name: {
-            required: 'Введіть ім`я',
-            minLength: 'Введіть мінімум 3 символи',
-            maxLength: 'Максимальна кількість символів - 15'
         },
-        tel: {
-            required: 'Введіть телефон',
-            function: 'Введіть коректний телефон'
-        }
-    },
-    colorWrong: 'red',
-    submitHandler: function (thisForm) {
-        let formData = new FormData(thisForm);
-        
-        let xhr = new XMLHttpRequest();
+        messages: {
+            name: {
+                required: 'Введіть ім`я',
+                minLength: 'Введіть мінімум 3 символи',
+                maxLength: 'Максимальна кількість символів - 15'
+            },
+            tel: {
+                required: 'Введіть телефон',
+                function: 'Введіть коректний телефон'
+            },
+            radio: {
+                required: 'Виберіть один із запропонованих пунктів'
+            }
+        },
+        colorWrong: 'red',
+        submitHandler: function (thisForm) {
+            let formData = new FormData(thisForm);
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    yes.classList.add('ok');
-                    setTimeout(
-                        () => {
-                            yes.classList.remove('ok');
-                        },
-                        2 * 1000
-                      );
+            let xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        setTimeout(
+                            () => {
+                                myElement.children[j].classList.remove('on');
+                                myElement.children[j + 1].classList.add('on');
+                                j++;
+                            },
+                            2 * 10
+                        );
+                    }
                 }
             }
+            xhr.open('POST', 'mail.php', true);
+            xhr.send(formData);
+
+            thisForm = reset();
         }
-        xhr.open('POST', 'mail.php', true);
-        xhr.send(formData);
+    });
+}
 
-        thisForm = reset();
-    }
-});
+if (mainForm !== null) {
+    new window.JustValidate('.form__profile', {
+        rules: {
+            tel: {
+                required: true,
+                function: () => {
+                    const phone = telMain.inputmask.unmaskedvalue();
+                    return Number(phone) && phone.length === 10;
+                }
+            }
+        },
+        messages: {
+            name: {
+                required: 'Введіть ім`я',
+                minLength: 'Введіть мінімум 3 символи',
+                maxLength: 'Максимальна кількість символів - 15'
+            },
+            tel: {
+                required: 'Введіть телефон',
+                function: 'Введіть коректний телефон'
+            }
+        },
+        colorWrong: 'red',
+        submitHandler: function (thisForm) {
+            let formData = new FormData(thisForm);
 
+            let xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        yes.classList.add('ok');
+                        setTimeout(
+                            () => {
+                                yes.classList.remove('ok');
+                            },
+                            2 * 1000
+                        );
+                    }
+                }
+            }
+            xhr.open('POST', 'mail.php', true);
+            xhr.send(formData);
+
+            thisForm = reset();
+        }
+    });
+}
